0473b20bb5f535bd4be60b7bdd65ac94ee3d8539
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-11 21:27:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,29 @@
+# require modules here
+
+require 'yaml'
+require 'pry'
+
+def load_library(file_path)
+  # code goes here
+  
+  emojis = YAML.load_file(file_path)
+  
+  new_hash = {}
+  
+  emojis.each do |key, value|
+    
+    new_hash[key]= {}
+    
+    new_hash[key][:english] = value[0]
+    
+    new_hash[key][:japanese] = value[1]
+    
+    
+  end
+  
+  new_hash
+  
+end
 
 
 def get_japanese_emoticon
