283bd4a67e8e67f754b592ec4569f0b554681631
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-11 21:27:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,26 @@
 # require modules here
 
-def load_library
+require 'yaml'
+require 'pry'
+
+def load_library(file_path)
   # code goes here
+  
+  emojis = YAML.load_file(file_path)
+  
+  emojis.each do |key, value|
+    
+    binding.pry
+    
+    
+    
+  
+  
+   # binding.pry
+    
+    
+  end
+  
 end
 
 def get_japanese_emoticon
